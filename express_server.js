e70a37e82bd896e7e10a901a2b53c9466eab6229
FAYZA DAOUDI <foufadaoudi@gmail.com>
2020-06-09 10:51:19 +0000

    reafactoring express_sever

--- express_server.js
+++ express_server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+  let randomString = Math.random().toString(36).slice(-6);
+  return randomString;
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -27,11 +35,30 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]  };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  let shortUrl = generateRandomString();
+  urlDatabase[shortUrl] = req.body.longURL;
+    //console.log(req.body);  // Log the POST request body to the console
+  res.redirect(`/urls/${shortUrl}`);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
